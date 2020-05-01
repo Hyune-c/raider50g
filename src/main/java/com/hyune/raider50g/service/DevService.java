ceb2b5763accc959be71941ae90baae29bfaeccf
Byung Hyun Choi <choi8608@gmail.com>
2020-05-01 16:06:59 +0900

    Refactor: Refactor parse body logic
    - getBody 를 String 으로 받는 것에서 json 을 사용하도록 변경
    
    #5

--- src/main/java/com/hyune/raider50g/service/DevService.java
+++ src/main/java/com/hyune/raider50g/service/DevService.java
@@ -2,13 +2,12 @@ package com.hyune.raider50g.service;
 
 import com.hyune.raider50g.config.ConfigureDiscordApi;
 import com.hyune.raider50g.property.Mock;
-import java.nio.charset.StandardCharsets;
 import lombok.extern.slf4j.Slf4j;
 import org.javacord.api.DiscordApi;
+import org.json.simple.JSONArray;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -25,10 +24,8 @@ public class DevService {
     this.discordApi = ConfigureDiscordApi.getDiscordApi();
   }
 
-  public String getChannelMessages() {
+  public JSONArray getChannelMessages() {
     HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(
-        new MediaType("application", "json", StandardCharsets.UTF_8));
     headers.add("Authorization", "Bot " + Mock.getBotToken());
 
     UriComponents uriComponents = UriComponentsBuilder
@@ -36,8 +33,8 @@ public class DevService {
         .path("/channels/" + Mock.CHANNEL_BLACKWING + "/messages")
         .build(false);
 
-    ResponseEntity<String> response = new RestTemplate().exchange(
-        uriComponents.toUri(), HttpMethod.GET, new HttpEntity<String>(headers), String.class);
+    ResponseEntity<JSONArray> response = new RestTemplate().exchange(
+        uriComponents.toUri(), HttpMethod.GET, new HttpEntity<String>(headers), JSONArray.class);
 
     return response.getBody();
   }
