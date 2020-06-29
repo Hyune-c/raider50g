7ba27f6e841173a435b0b1ac3521831a47b75c62
Byung Hyun Choi <choi8608@gmail.com>
2020-06-29 23:50:38 +0900

    feat: find Booking 구현
    - 해당 날짜에 취소되지 않은 예약만 가져오도록 구현

--- src/main/java/com/hyune/raider50g/repository/BookingRepository.java
+++ src/main/java/com/hyune/raider50g/repository/BookingRepository.java
@@ -1,6 +1,10 @@
 package com.hyune.raider50g.repository;
 
 import com.hyune.raider50g.domain.booking.Booking;
+import com.hyune.raider50g.domain.booking.QBooking;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDate;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -16,5 +20,15 @@ public class BookingRepository {
   public Booking save(Booking booking) {
     em.persist(booking);
     return booking;
+  }
+
+  public List<Booking> find(LocalDate findDate) {
+    JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+    QBooking booking = QBooking.booking;
+
+    return queryFactory.selectFrom(booking)
+        .where(booking.raidInfo.raidDate.eq(findDate)
+            , booking.raidInfo.cancel.isFalse())
+        .fetch();
   }
 }
