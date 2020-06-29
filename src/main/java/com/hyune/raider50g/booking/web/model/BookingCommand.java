8846533ffade5790c7fbd35d427996077717ce87
Byung Hyun Choi <choi8608@gmail.com>
2020-06-29 19:21:28 +0900

    refactor: DTO 를 어노테이션으로 정리
    - DTO 를 어노테이션으로 정리

--- src/main/java/com/hyune/raider50g/booking/web/model/BookingCommand.java
+++ src/main/java/com/hyune/raider50g/booking/web/model/BookingCommand.java
@@ -2,20 +2,16 @@ package com.hyune.raider50g.booking.web.model;
 
 import com.hyune.raider50g.booking.data.RaidInfo;
 import com.hyune.raider50g.booking.data.Raider;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public class BookingCommand {
 
   private RaidInfo raidInfo;
   private Raider raider;
-
-  @Builder
-  public BookingCommand(RaidInfo raidInfo, Raider raider) {
-    this.raidInfo = raidInfo;
-    this.raider = raider;
-  }
 }
