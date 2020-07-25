064299e473c6f80782ddb10ded610aaa3febae84
Byung Hyun Choi <choi8608@gmail.com>
2020-07-25 20:09:25 +0900

    refactor: createBookingSheet() 에 notice 추가
    - createBookingSheet() 에 notice 추가

--- src/main/java/com/hyune/raider50g/domain/booking/dto/BookingList.java
+++ src/main/java/com/hyune/raider50g/domain/booking/dto/BookingList.java
@@ -52,15 +52,18 @@ public class BookingList {
   }
 
   public String createBookingSheet() {
+    String notice = String.format("손님팟 룰 : %s",
+        "https://discordapp.com/channels/256380741229871104/684266401661583439/730015381598502963");
     String title = String.format("%s\t%s (일) PM 19:00\t%d/40"
         , raidInfo.getDungeonType().getName()
         , raidInfo.getRaidDate()
         , bookingCount());
-    String category = "직업 현재/최대\t예약 인원\t(2~3탱전 11힐)";
+    String category = "직업 현재/최대\t예약 인원\t(2~3탱전 10힐 4~5손님)";
     String contents = Arrays.stream(ClassType.values())
         .map(this::makeClassLine)
         .collect(Collectors.joining("\n", "", "\n"));
 
-    return String.format("```%s``````%s``````%s```", title, category, contents);
+
+    return String.format("%s\n\n", notice) + String.format("```%s``````%s``````%s```", title, category, contents);
   }
 }
