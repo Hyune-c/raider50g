63bb1c5064e2ec105d5fa7bcf97eb9b547475c76
Byung Hyun Choi <choi8608@gmail.com>
2020-08-01 18:31:14 +0900

    refactor: MultiValueMap 을 HashMap 으로 개선
    - MultiValueMap 은 Spring 에 의존적인 컬렉션이므로 Java 표준 컬렉션인 HashMap 으로 개선하였습니다.

--- src/main/java/com/hyune/raider50g/domain/booking/dto/RaiderList.java
+++ src/main/java/com/hyune/raider50g/domain/booking/dto/RaiderList.java
@@ -2,43 +2,45 @@ package com.hyune.raider50g.domain.booking.dto;
 
 import com.hyune.raider50g.common.type.ClassType;
 import com.hyune.raider50g.domain.booking.Booking;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import lombok.Builder;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
 @Builder
 public class RaiderList {
 
-  private MultiValueMap<ClassType, String> descList;
+  private HashMap<ClassType, ArrayList<String>> descMap;
 
   public static RaiderList of(List<Booking> bookings) {
-    MultiValueMap<ClassType, String> descList = new LinkedMultiValueMap<>();
-    bookings.forEach(booking -> descList.add(booking.getClassType(), booking.getUserName()));
+    HashMap<ClassType, ArrayList<String>> descMap = new HashMap<>();
+    Arrays.stream(ClassType.values())
+        .forEach(classType -> descMap.put(classType, new ArrayList<>()));
+    bookings.forEach(booking -> descMap.get(booking.getClassType()).add(booking.getUserName()));
 
     return RaiderList.builder()
-        .descList(descList)
+        .descMap(descMap)
         .build();
   }
 
   public int size() {
-    return descList.keySet().stream()
-        .mapToInt(key -> descList.get(key).size())
+    return descMap.keySet().stream()
+        .mapToInt(key -> descMap.get(key).size())
         .reduce(Integer::sum)
         .orElse(0);
   }
 
   public int size(ClassType classType) {
-    return (Objects.isNull(descList.get(classType)))
+    return (Objects.isNull(descMap.get(classType)))
         ? 0
-        : descList.get(classType).size();
+        : descMap.get(classType).size();
   }
 
   public List<String> getRaiders(ClassType classType) {
-    return Optional.ofNullable(descList.get(classType))
-        .orElse(Collections.emptyList());
+    return Optional.ofNullable(descMap.get(classType))
+        .orElse(new ArrayList<>());
   }
 }
