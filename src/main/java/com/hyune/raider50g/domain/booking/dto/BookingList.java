fdff14e5861effd01647d5f9d25306b3a99a3adf
Byung Hyun Choi <choi8608@gmail.com>
2020-08-01 15:07:31 +0900

    refactor: BookingList 리팩토링
    - 공통
      - @Getter, @AllArgsConstructor 삭제
      - 메소드를 세분화
    - BookingList
      - createBookingSheet() 의 분리
      - String 객체들의 final 선언 및 분리
    - RaiderList
      - raiderCount() 에서 size() 로 메소드명 변경

--- src/main/java/com/hyune/raider50g/domain/booking/dto/BookingList.java
+++ src/main/java/com/hyune/raider50g/domain/booking/dto/BookingList.java
@@ -6,17 +6,13 @@ import com.hyune.raider50g.domain.booking.RaidInfo;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
 @Builder
-@AllArgsConstructor
 public class BookingList {
 
-  private final RaidInfo raidInfo;
-  private final RaiderList raiderList;
+  private RaidInfo raidInfo;
+  private RaiderList raiderList;
 
   public static BookingList of(RaidInfo raidInfo, List<Booking> bookings) {
     return BookingList.builder()
@@ -26,22 +22,45 @@ public class BookingList {
   }
 
   private int bookingCount() {
-    return raiderList.raiderCount();
+    return raiderList.size();
   }
 
   public String createBookingSheet() {
-    String notice = String.format("손님팟 룰 : %s",
-        "https://discordapp.com/channels/256380741229871104/684266401661583439/730015381598502963");
-    String title = String.format("%s\t%s (일) PM 19:00\t%d/40"
-        , raidInfo.getDungeonType().getName()
-        , raidInfo.getRaidDate()
-        , bookingCount());
-    String category = "직업 현재/최대\t예약 인원\t(2~3탱전 10힐 4~5손님)";
-    String contents = Arrays.stream(ClassType.values())
-        .map(raiderList::makeClassLine)
-        .collect(Collectors.joining("\n", "", "\n"));
+    final String CATEGORY = "직업 현재/최대\t예약 인원\t(2~3탱전 10힐 4~5손님)";
+    final String BOOKING_SHEET_FORMAT = "%s\n\n```%s``````%s``````%s```";
+    return String.format(BOOKING_SHEET_FORMAT
+        , getNotice(), getTitle(), CATEGORY, getContent());
+  }
 
-    return String.format("%s\n\n", notice) + String
-        .format("```%s``````%s``````%s```", title, category, contents);
+  private String getContent() {
+    final String CONTENT_FORMAT = "%s\t%s";
+    return Arrays.stream(ClassType.values())
+        .map(classType -> String.format(CONTENT_FORMAT
+            , classHeader(classType), classBody(classType)))
+        .collect(Collectors.joining("\n", "", "\n"));
+  }
+
+  private String getTitle() {
+    final String TITLE_FORMAT = "%s\t%s (일) PM 19:00\t%d/40";
+    return String.format(TITLE_FORMAT
+        , raidInfo.getDungeonType().getName(), raidInfo.getRaidDate(), bookingCount());
+  }
+
+  private String getNotice() {
+    final String NOTICE_FORMAT = "손님팟 룰 : %s";
+    final String NOTICE_CONTENT = "https://discordapp.com/channels/256380741229871104/684266401661583439/730015381598502963";
+    return String.format(NOTICE_FORMAT, NOTICE_CONTENT);
+  }
+
+  private String classHeader(ClassType classType) {
+    final String CLASS_HEADER_FORMAT = "%s (%d/%d)";
+    return String.format(CLASS_HEADER_FORMAT
+        , classType.getName(), raiderList.size(classType), classType.getMaxCount());
+  }
+
+  private String classBody(ClassType classType) {
+    return (raiderList.size(classType) == 0)
+        ? ""
+        : String.join("\t", raiderList.getRaiders(classType));
   }
 }
